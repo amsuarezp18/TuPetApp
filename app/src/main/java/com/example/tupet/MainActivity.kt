1d65d5191d4f941f97086ec34dda7cfd35b45aca
amsuarezp <amsuarezp@gmail.com>
2021-04-26 20:54:43 -0500

    funciona cn

--- app/src/main/java/com/example/tupet/MainActivity.kt
+++ app/src/main/java/com/example/tupet/MainActivity.kt
@@ -4,12 +4,47 @@ import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.tupet.databinding.ActivityMainBinding
+import com.example.tupet.fragments.AccountFragment
+import com.example.tupet.fragments.FindFragment
+import com.example.tupet.fragments.LocationFragment
+import com.example.tupet.fragments.MedicalFragment
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+    var mapOpened: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-      
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val firstFragment = AccountFragment()
+        val secondFragment = FindFragment()
+        val thirdFragment =  LocationFragment()
+        val fourthFragment = MedicalFragment()
+
+        setCurrentFragment(firstFragment)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.findPet -> setCurrentFragment(secondFragment)
+                R.id.locationMap -> setCurrentFragment(thirdFragment)
+                R.id.medical -> setCurrentFragment(fourthFragment)
+                R.id.account -> setCurrentFragment(firstFragment)
+            }
+            true
+        }
+        
+    }
+
+    private fun setCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
     }
 }
