5615697c4aa0baa4e957893b8875b0f76569720d
amsuarezp <amsuarezp@gmail.com>
2021-04-27 06:38:12 -0500

    sistema recoemndador

--- app/src/main/java/com/example/tupet/fragments/RecommendFragment.kt
+++ app/src/main/java/com/example/tupet/fragments/RecommendFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tupet.R
+import com.example.tupet.databinding.FragmentRecommendBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,15 +20,11 @@ private const val ARG_PARAM2 = "param2"
  */
 class RecommendFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentRecommendBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,7 +32,45 @@ class RecommendFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recommend, container, false)
+        _binding = FragmentRecommendBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val btnrecommend = binding.recomendacionbtn
+
+        val checkOne = binding.checkboxOne
+        val checkTwo = binding.checkboxTwo
+        val checkTree = binding.checkboxTwoOne
+        val checkFour = binding.checkboxTwoTwo
+        val checkFive = binding.checkboxTreeOne
+        val checkSix = binding.checkboxTreeTwo
+        val checkSeven = binding.checkboxTreeTree
+        val checkEight = binding.checkboxTreeFour
+        val checkNine = binding.checkboxFourOne
+        val checkTen = binding.checkboxFourTwo
+        val checkEleven = binding.checkboxFourTree
+        val checkTwelve = binding.checkboxFiveOne
+        val checkThirteen = binding.checkboxFiveTwo
+        val checkFourteen = binding.checkboxFiveTree
+
+        btnrecommend.setOnClickListener{
+
+            if( checkTwo.isChecked  ){
+
+            }
+            else if(3==3){
+
+            }
+            else if(4 == 4){
+
+            }
+            else {
+
+            }
+
+
+        }
+
+        return view
     }
 
     companion object {
