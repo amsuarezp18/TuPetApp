ece8f88161cac34ab383d9946fe1f28a6be35438
amsuarezp <amsuarezp@gmail.com>
2021-04-27 17:53:09 -0500

    map

--- app/src/main/java/com/example/tupet/fragments/LocationFragment.kt
+++ app/src/main/java/com/example/tupet/fragments/LocationFragment.kt
@@ -1,16 +1,16 @@
 package com.example.tupet.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import com.example.tupet.MapsActivity
 import com.example.tupet.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.tupet.databinding.FragmentLocationBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
  * A simple [Fragment] subclass.
@@ -18,16 +18,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LocationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    var mapOpened: Boolean = false
+
+    private var _binding: FragmentLocationBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +34,35 @@ class LocationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_location, container, false)
+
+        var btnMap =  view.findViewById(R.id.navegarBtn) as AppCompatButton
+
+        btnMap.setOnClickListener{
+            mapOpened = true
+            val intent = Intent(this.activity, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LocationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LocationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
+    override fun onResume() {
+        super.onResume()
+        if(mapOpened){
+            parentFragmentManager.beginTransaction().apply{
+                replace(R.id.flFragment, LocationFragment())
+                commit()
+            }
+            mapOpened=false
+        }
+    }
+
+
 }
